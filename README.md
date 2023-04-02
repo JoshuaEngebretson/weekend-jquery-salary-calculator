ee912650a428ad34a6c31c883401aa97f919b9b9
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-04-01 19:53:33 -0500

    Wrote README.md added removeEmployeeInfo
    README now includes a Description, Usage, Built With, Acknowledgement, and Support
    
    Script.js changes
    	-Added removeEmployeeInfo function that deletes that line of employee information from the DOM.
    	-Started basis of recalculating Total Monthly budget based on no longer including the removed employee.

--- README.md
+++ README.md
@@ -1,9 +1,40 @@
-# Project Name
+# Joshua Inc. Salaray Calc (Weekend Challenge: jQuery Salary Calculator)
 
-[Project Instructions](./INSTRUCTIONS.md), this line may be removed once you have updated the README.md
 
 ## Description
+Duration: Weekend Challenge (03/31/23-04/02/23)
 
-Your project description goes here. What problem did you solve? How did you solve it?
+This monthly salary calculator takes in employee information and returns a table with the given employees all while calculating the total monthly budget remaining.
 
-Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
+
+## Usage
+This calculator starts with a base monthly budget of $20,000.00
+
+**To add an employee**
+1. Complete the "Add Employee" form at the top by entering
+    *  First name
+    * Last name
+    * ID Number
+    * Job title
+    * Annual Salary
+2. Clicking the "Submit" button
+
+* Note, if the Total Budget exceeds $20,000.00 a visual alert will appear on the user interface. The "Total Monthly" line will turn red \(`#FF0000`)
+
+**To remove an employee**
+1. Click the "Delete" button on the line that you want to remove.
+
+
+## Built With
+* jQuery library
+
+
+## Acknowledgement
+To Matt Black and Andrew Harasymiw,
+* Thank you for being wonderful instructors that helped get me through my first full-time week of the Full Stack Development course with [Prime Digital Academy](https://www.primeacademy.io/).
+
+To my partner Brooke,
+* Thank you for supporting me through this programming journey. I would not be able to make this work without you.
+
+## Support
+If you have suggestions or issues, please email me [here](mailto:joshua.engebretson@gmail.com).
--- script.js
+++ script.js
@@ -4,6 +4,8 @@ $(document).ready(onReady);
 //Global Variables
 let monthlyBudget = '20000.00';
 let EmployeeArray = [];
+let EmployeeData_Array =[];
+
 
 //Setting Global Variable that converts a given number
 //  US Currency
@@ -22,10 +24,18 @@ function onReady() { //Sourced and working
     //Send monthlyBudget variable to the DOM
     budget.append(USDollar.format(monthlyBudget));
 
-    //Create listener for submit button
-    //  Calls on function to add Employee Info
-    //  to the table on the DOM
-    $('#Submit-Employee-Info').on('click', appendEmployeeInfo);
+    //Create listener for click even on submit button
+    //  Calls on function to add Employee Info to
+    //  the table on the DOM
+    $('#Submit-Employee-Info').on('click',appendEmployeeInfo);
+
+    //Create listener for click event on delete button
+    //  Calls on function to remove that line's 
+    //  employee data from the table
+        // Will add functionality to add their salary
+        //  back to the total remaining monthlyBudget
+    $('#Employee-Data').on('click', 
+        '.Delete_EmployeeData', removeEmployeeInfo);
 
 
 } //End onReady
@@ -41,32 +51,40 @@ function appendEmployeeInfo(event) {
     //  form element
     event.preventDefault();
 
-    //Create object to hold captured employee data
-    let nextEmployee = {
-        firstName: $('#first-Name').val(),
-        lastName: $('#last-Name').val(),
-        ID_Number: $('#ID-Number').val(),
-        jobTitle: $('#job-Title').val(),
-        AnnualSalary: Number($('#Annual-Salary').val()), // Change to number
-    };
+    //Wrap everything in an if statement that requires all
+    //  form data fields to be filled before processing more
+    //  of this function.
+    if ($('#first-Name').val() != '' && $('#last-Name').val() != '' &&
+        $('#ID-Number').val() != '' && $('#job-Title').val() != '' &&
+        $('#Annual-Salary').val() != '') {
+          
+        //Create object to hold captured employee data
+        let nextEmployee = {
+            firstName: $('#first-Name').val(),
+            lastName: $('#last-Name').val(),
+            ID_Number: $('#ID-Number').val(),
+            jobTitle: $('#job-Title').val(),
+            AnnualSalary: Number($('#Annual-Salary').val()), // Change to number
+        };
 
-    //Store nextEmployee data in EmployeeArray to later
-    //  call upon and place inside of table on DOM
-    EmployeeArray.push(nextEmployee);
+        //Store nextEmployee data in EmployeeArray to later
+        //  call upon and place inside of table on DOM
+        EmployeeArray.push(nextEmployee);
+        EmployeeData_Array.push(Object.values(nextEmployee))
 
-    //Reset input fields on form to placeholder text
-    $('#first-Name').val('');
-    $('#last-Name').val('');
-    $('#ID-Number').val('');
-    $('#job-Title').val('');
-    $('#Annual-Salary').val('');
+        //Reset input fields on form to placeholder text
+        $('#first-Name').val('');
+        $('#last-Name').val('');
+        $('#ID-Number').val('');
+        $('#job-Title').val('');
+        $('#Annual-Salary').val('');
 
-    //Call function to calculate remaining budget
-    MonthlyBudgetRemaining();
+        //Call function to calculate remaining budget
+        MonthlyBudgetRemaining();
 
-    //Place Employee data on the DOM
-    ShowEmployees();
-
+        //Place Employee data on the DOM
+        ShowEmployees();
+    } //End if Form not complete
 
 } //End appendEmployeeInfo
 
@@ -96,7 +114,7 @@ function MonthlyBudgetRemaining() {
     //If total sumAllSalaries is  than $20000
     //  set background-color to red
     if (sumAllSalaries > 20000) {
-        $('.budget').css('background-color', 'red')
+        $('.budget').css('background-color', '#FF0000')
     }
 
     //Update location with remainingMonthlyBudget.
@@ -120,14 +138,18 @@ function ShowEmployees() {
     //Creates a delete button after each employees data.
     for (const employee of EmployeeArray) {
         EmployeeInfo.append(`
-            <tr>
+            <tr data-firstName="${employee.firstName}"
+              data-lastName="${employee.lastName}"
+              data-ID="${employee.ID_Number}"
+              data-job="${employee.jobTitle}"
+              data-salary="${Intl.NumberFormat().format(employee.AnnualSalary)}">
                 <td>${employee.firstName}</td>
                 <td>${employee.lastName}</td>
                 <td>${employee.ID_Number}</td>
                 <td>${employee.jobTitle}</td>
                 <td class="right">${USDollar.format(employee.AnnualSalary)}</td>
                 <td>
-                    <button class "Delete_EmployeeData">Delete</button>
+                    <button class="Delete_EmployeeData">Delete</button>
                 </td>
             </tr>
         `);
@@ -138,5 +160,41 @@ function ShowEmployees() {
         <tr class="empty-fill"></tr>
     `)
 
+} //End ShowEmployees
 
-}
+
+function removeEmployeeInfo(){
+
+    console.log('in removeEmployeeInfo');
+
+    // Target this, then go up 2 levels and delete
+    //  the whole row worth of this employee's data.
+
+
+    // Grab data from what was just deleted.
+    let removedData = (Object.values(($(this).parent().parent()).data()).reverse())
+
+    let removedEl;
+
+    //Remove Employee's data from EmployeeArray and update
+    //  DOM to reflect budget change.
+    //Use for...of loop to cycle through array and remove employee
+    //  data that matches removedData (removed employee).
+    for (const employee of EmployeeArray) {
+        if (employee.firstName == removedData[0] && employee.lastName == removedData[1]
+            && employee.ID_Number == removedData [2] && employee.jobTitle == removedData [3]
+            && employee.AnnualSalary == removedData[4]) {
+            EmployeeArray.splice(employee, 1);
+            console.log('splicing');
+            console.log(removedEl);
+            break
+        }
+    } //End for...of
+
+    console.log(removedData);
+    console.log(EmployeeArray);
+
+    MonthlyBudgetRemaining()
+    $(this).parent().parent().remove();
+
+} //End removeEmployeeInfo
